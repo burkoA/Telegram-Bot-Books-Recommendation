131202315a8936058d95b3293c5a871b25754e8e
Arsen Burko <arsenburko67@gmail.com>
2024-09-19 12:58:12 +0200

    Complete Author Search

--- BooksRec/Program.cs
+++ BooksRec/Program.cs
@@ -18,10 +18,13 @@ namespace BooksRec
         private static User me;
         private static Random random = new Random();
 
+        private static string currentOperation = null;
+        private static List<Book> booksList = new List<Book>();
+
         static async Task Main(string[] args)
         {
             var cts = new CancellationTokenSource();
-            var botOptions = new TelegramBotClientOptions("7510414878:AAGGTwL5_8Wd0deslAHQ2x-hW_hKwsh7LHo");
+            var botOptions = new TelegramBotClientOptions("TOKEN");
             bot = new TelegramBotClient(botOptions);
 
             me = await bot.GetMeAsync();
@@ -39,8 +42,6 @@ namespace BooksRec
         {
             Console.WriteLine(exception);
         }
-
-        private static string currentOperation = null;
 
         private static async Task OnMessage(Message msg, UpdateType type)
         {
@@ -82,37 +83,9 @@ namespace BooksRec
                 else
                 {
                     // Query the database to find books by the given author
-                    var resultBooks = _db.Books.Where(r => r.Author.Contains(authorName)).ToList();
-                    string resultMessage = "Here are the books by the author:\n";
+                    booksList = _db.Books.Where(r => r.Author.Contains(authorName)).ToList();
 
-                    if (resultBooks.Count == 1)
-                    {
-                        resultMessage += $"Book title - {resultBooks[0].Title} \n" +
-                                                $"Book author - {resultBooks[0].Author} \n" +
-                                                $"Book genres - {resultBooks[0].Genres} \n" +
-                                                $"Book published year - {resultBooks[0].Year} \n" +
-                                                $"Choose next option: \n";
-
-                        await bot.SendTextMessageAsync(msg.Chat, resultMessage, replyMarkup: new InlineKeyboardMarkup().AddButtons("Repeat this author", "Write another").AddNewRow().AddButton("Came back to /start"));
-                    }
-                    else if(resultBooks.Count > 1)
-                    {
-                        int randomBook = random.Next(0, resultBooks.Count);
-
-                        resultMessage += $"Book title - {resultBooks[randomBook].Title} \n" +
-                                                $"Book author - {resultBooks[randomBook].Author} \n" +
-                                                $"Book genres - {resultBooks[randomBook].Genres} \n" +
-                                                $"Book published year - {resultBooks[randomBook].Year} \n" +
-                                                $"Choose next option: \n";
-
-                        resultBooks.Remove(resultBooks[randomBook]);
-
-                        await bot.SendTextMessageAsync(msg.Chat, resultMessage, replyMarkup: new InlineKeyboardMarkup().AddButtons("Repeat this author", "Write another").AddNewRow().AddButton("Came back to /start"));
-                    }
-                    else
-                    {
-                        await bot.SendTextMessageAsync(msg.Chat, "No books found for this author.");
-                    }
+                    await GetBookByAuthor(booksList, booksList.Count,msg);
                 }
 
                 // Reset the operation after completion
@@ -131,14 +104,60 @@ namespace BooksRec
                 await bot.AnswerCallbackQueryAsync(query.Id, $"You picked {query.Data}");
                 await bot.SendTextMessageAsync(query.Message!.Chat, $"User {query.From} clicked on {query.Data}");
 
-                if (query.Data.Contains("author"))
+                if (query.Data!.Contains("author") || query.Data.Contains("another"))
                 {
                     // Set the operation context
                     currentOperation = "find_by_author";
 
                     // Ask the user to provide the author's name
                     await bot.SendTextMessageAsync(query.Message.Chat, "Please write the author's name:");
+                } 
+                else if (query.Data.Contains("Repeat"))
+                {
+                    await GetBookByAuthor(booksList, booksList.Count, query.Message);
                 }
+                else if (query.Data.Contains("/start"))
+                {
+                    Message msg = new Message()
+                    {
+                        Chat = query.Message.Chat,
+                        Text = "/start"
+                    };
+
+                    await OnMessage(msg, update.Type);
+                }
+            }
+        }
+
+        private static async Task GetBookByAuthor(List<Book> result, int count, Message msg)
+        {
+            string resultMessage = "Here are the books by the author:\n";
+
+            if (result.Count == 1)
+            {
+                resultMessage += $"Book title - {result[0].Title} \n" +
+                                        $"Book author - {result[0].Author} \n" +
+                                        $"Book genres - {result[0].Genres} \n" +
+                                        $"Book published year - {result[0].Year} \n" +
+                                        $"Choose next option: \n";
+
+                await bot.SendTextMessageAsync(msg.Chat, resultMessage, replyMarkup: new InlineKeyboardMarkup().AddButtons("Repeat (books can repeat)", "Write another").AddNewRow().AddButton("Came back to /start"));
+            }
+            else if (result.Count > 1)
+            {
+                int randomBook = random.Next(0, result.Count);
+
+                resultMessage += $"Book title - {result[randomBook].Title} \n" +
+                                        $"Book author - {result[randomBook].Author} \n" +
+                                        $"Book genres - {result[randomBook].Genres} \n" +
+                                        $"Book published year - {result[randomBook].Year} \n" +
+                                        $"Choose next option: \n";
+
+                await bot.SendTextMessageAsync(msg.Chat, resultMessage, replyMarkup: new InlineKeyboardMarkup().AddButtons("Repeat (books can repeat)", "Write another").AddNewRow().AddButton("Came back to /start"));
+            }
+            else
+            {
+                await bot.SendTextMessageAsync(msg.Chat, "No books found for this author.", replyMarkup: new InlineKeyboardMarkup().AddButton("Write another").AddNewRow().AddButton("Came back to /start"));
             }
         }
     }
